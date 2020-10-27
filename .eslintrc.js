4d4d675cb55e19446d14ad040f0f33eb4e4bd7c8
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2020-10-27 22:07:27 +0900

    Fix update settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+const OFF = 0
+const WARNING = 1
+const ERROR = 2
+
 module.exports = {
   env: {
     browser: true,
@@ -32,8 +36,8 @@ module.exports = {
         argsIgnorePattern: '^_',
       }
     ],
-    'no-return-assign': 0,
-    'no-console': 2,
+    'no-return-assign': OFF,
+    'no-console': ERROR,
     'import/no-extraneous-dependencies': [
       'error',
       {
@@ -59,15 +63,17 @@ module.exports = {
       ],
       'newlines-between': 'always',
     }],
-    'import/no-named-export': 0,
-    'import/newline-after-import': 2,
-    'import/named': 0, // ref: https://github.com/benmosher/eslint-plugin-import/issues/1282
-    'import/prefer-default-export': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'react/display-name': 0,
+    'import/no-named-export': OFF,
+    'import/newline-after-import': ERROR,
+    'import/named': OFF, // ref: https://github.com/benmosher/eslint-plugin-import/issues/1282
+    'import/prefer-default-export': OFF,
+    'jsx-a11y/anchor-is-valid': OFF,
+    'react/display-name': OFF,
     'react-hooks/rules-of-hooks': 'error',
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-    'react/prop-types': 0,
+    'react/jsx-filename-extension': [WARNING, { extensions: ['.tsx'] }],
+    'react/jsx-fragments': ['error', 'element'],
+    'react/jsx-props-no-spreading': OFF,
+    'react/prop-types': OFF,
   },
   settings: {
     react: {
