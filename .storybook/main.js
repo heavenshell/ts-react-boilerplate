55d03c9c66f923acf607459bb10b4bdeb6b2ee0c
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2021-01-04 00:35:47 +0900

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-const TsConfigWebpackPlugin = require('ts-config-webpack-plugin')
+const workers = require('os').cpus().length - 1
 
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx'],
@@ -15,8 +15,27 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.tsx?$/,
+        use: [
+          { loader: 'cache-loader' },
+          {
+            loader: 'thread-loader',
+            options: {
+              // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+              workers,
+              poolTimeout: Infinity, // set this to Infinity in watch mode - see https://github.com/webpack-contrib/thread-loader
+            },
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+            },
+          },
+        ],
+      },
     )
-    config.plugins.push(new TsConfigWebpackPlugin())
     config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx')
     return config
   }
