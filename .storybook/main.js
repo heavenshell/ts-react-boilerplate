7e6d2f60eb984f17ca2277ce1aa2d3440245c39a
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2021-01-05 01:41:46 +0900

    Fix prepare for webpack5 (#251)
    * Fix prepare for webpack5
    
    * Fix storybook settings
    
    * Fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
-const TsConfigWebpackPlugin = require('ts-config-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+const cpus = require('os').cpus().length
+const tsLoaderWorkers = cpus > 3 ? cpus - 2 : 1
 
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx'],
@@ -15,8 +18,31 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'cache-loader',
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+              transpileOnly: true,
+            },
+          },
+          {
+            // run compilation threaded
+            loader: 'thread-loader',
+            options: {
+              // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+              workers: tsLoaderWorkers,
+            },
+          },
+        ],
+      },
     )
-    config.plugins.push(new TsConfigWebpackPlugin())
+    config.plugins.push(new ForkTsCheckerWebpackPlugin({ async: true }))
     config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx')
     return config
   }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,14 @@
-import { Configuration } from 'webpack'
-import { default as HtmlWebpackPlugin } from 'html-webpack-plugin'
-import { default as MiniCssExtractPlugin } from 'mini-css-extract-plugin'
-import { default as TerserPlugin } from 'terser-webpack-plugin'
-import { default as OptimizeCSSAssetsPlugin } from 'optimize-css-assets-webpack-plugin'
+import os from 'os'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const TsConfigWebpackPlugin = require('ts-config-webpack-plugin')
+import { Configuration } from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+
+const cpus = os.cpus().length
+const tsLoaderWorkers = cpus > 3 ? cpus - 2 : 1
 
 const config: Configuration = {
   devtool: 'source-map',
@@ -17,8 +20,40 @@ const config: Configuration = {
     path: `${__dirname}/dist`,
     publicPath: '/',
   },
+  devServer: {
+    historyApiFallback: {
+      rewrites: [{ from: /^\/*/, to: '/index.html' }],
+    },
+    host: '0.0.0.0',
+    port: 3000,
+    hot: true,
+    liveReload: true,
+  },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'cache-loader',
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+              transpileOnly: true,
+            },
+          },
+          {
+            // run compilation threaded
+            loader: 'thread-loader',
+            options: {
+              // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+              workers: tsLoaderWorkers,
+            },
+          },
+        ],
+      },
       {
         test: /\.less$/,
         use: [
@@ -43,7 +78,10 @@ const config: Configuration = {
     minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
   },
   plugins: [
-    new TsConfigWebpackPlugin(),
+    // new TsConfigWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      async: true,
+    }),
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
